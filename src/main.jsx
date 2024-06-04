cca3fcff847dc56242595d0758a6a5e31b4f9671
Oksana Tanashchuk <oksana.tanashuk@gmail.com>
2024-06-04 18:20:03 +0200

    add

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,17 @@ import App from "./App.jsx";
 import "./index.css";
 import { Provider } from "react-redux";
 import { store } from "./redux/store.js";
+import { BrowserRouter } from "react-router-dom";
+import { HelmetProvider } from "react-helmet-async";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
-    </Provider>{" "}
+      <BrowserRouter>
+        <HelmetProvider>
+          <App />
+        </HelmetProvider>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
